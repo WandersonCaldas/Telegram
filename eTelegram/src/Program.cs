864a6d93ee28d69f70447361215a48001f04a75d
Wanderson Caldas <wcaldasti@gmail.com>
2023-03-14 23:56:37 -0300

    FIRST COMMIT;

---
+++ eTelegram/src/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace src;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+class Program
+{    
+    static async Task Main(string[] args)
+    {
+        var botClient = new TelegramBotClient("CHAVE DO SEU BOT");
+        var chatId = ""; //Id da conversa
+        var message = "Data:" + DateTime.Now;
+
+        await botClient.SendTextMessageAsync(chatId, message);
+    }
+}
